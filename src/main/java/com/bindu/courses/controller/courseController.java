71790de314d9b36cb93cb4d0a7765526ed3ce26f
satya <satya@e6edf6fb-f266-4316-afb4-e53d95876a76>
2017-07-29 11:03:52 +0000

    Build update

--- src/main/java/com/bindu/courses/controller/courseController.java
+++ src/main/java/com/bindu/courses/controller/courseController.java
@@ -14,7 +14,7 @@ import com.bindu.courses.dao.CourseDAO;
 import com.bindu.courses.entity.Course;
 
 @Controller
-public class courseController {
+public class courseController 
 	
 
 	 @Autowired  
